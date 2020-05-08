b419d8bacc50bdc8a3da943e92cf3f5a8f6b8b1f
raphael <norlandrhagen@gmail.com>
2020-05-08 15:38:18 -0700

    fixed dbConnect pyodbc error

--- src/DB.py
+++ src/DB.py
@@ -29,7 +29,7 @@ def DB_modify(cmnd,server='Rainer'):
 # 	return dframe
 
 def dbRead(query,server='Rainer'):
-	conn = olddbConnect(server)
+	conn,cursor = dbConnect(server)
 	df = sql.read_sql(query, conn)
 	conn.close()
 	return df
@@ -48,28 +48,6 @@ def server_select_credentials(server):
 
     return usr,psw,ip,port
 
-def olddbConnect(server,db='Opedia', TDS_Version='7.3'):
-    if server == 'Rainier':
-        usr=cr.usr_rainier
-        psw=cr.psw_rainier
-        ip=cr.ip_rainier
-        port = cr.port_rainier
-    else:
-        usr=cr.usr_beast
-        psw=cr.psw_beast
-        ip=cr.ip_beast
-        port = cr.port_beast
-
-    server = ip + ',' + port
-
-    if platform.system().lower().find('windows') != -1:
-        conn = pyodbc.connect('DRIVER={SQL Server};SERVER=' + server + ';DATABASE=' + db + ';Uid=' + usr + ';Pwd='+ psw )
-    elif platform.system().lower().find('darwin') != -1:
-        conn = pyodbc.connect('DRIVER=/usr/local/lib/libtdsodbc.so;SERVER=' + server + ';DATABASE=' + db + ';Uid=' + usr + ';Pwd='+ psw )
-    elif platform.system().lower().find('linux') != -1:
-        conn = pyodbc.connect(DRIVER='/usr/lib/x86_64-linux-gnu/odbc/libtdsodbc.so', TDS_Version =  TDS_Version , server =  ip , port =  port, uid = usr, pwd = psw)
-
-    return conn
 def dbConnect(server,db='Opedia', TDS_Version='7.3'):
     usr,psw,ip,port = server_select_credentials(server)
 
@@ -82,6 +60,7 @@ def dbConnect(server,db='Opedia', TDS_Version='7.3'):
     elif platform.system().lower().find('linux') != -1:
         conn = pyodbc.connect(DRIVER='/usr/lib/x86_64-linux-gnu/odbc/libtdsodbc.so', TDS_Version =  TDS_Version , server =  ip , port =  port, uid = usr, pwd = psw)
     cursor = conn.cursor()
+
     return conn, cursor
 
 
