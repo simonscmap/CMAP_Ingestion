ab8e98c23d2a69fd199a27aa2c8433f90c917525
raphael <norlandrhagen@gmail.com>
2020-05-08 10:17:19 -0700

    more tests for common.py

--- tests/test_common.py
+++ tests/test_common.py
@@ -19,3 +19,20 @@ def test_nanToNA():
     expected_df = pd.DataFrame({'no_nans': ['1',3],'nans': ['2','']})
     func_df = cmn.nanToNA(test_df)
     assert_frame_equal(func_df, expected_df,obj = "nan to None test failed")
+
+def test_lowercase_List():
+    test_list = ['Asdf','SeaGlider', 'CMAP']
+    expected_test_list = ['asdf','seaglider', 'cmap']
+    func_list = cmn.lowercase_List(test_list)
+    assert func_list == expected_test_list, "lowercase_List test failed"
+
+def test_getColBounds():
+    test_df = pd.DataFrame({'col_1': [2,'',4,8]})
+    expected_min_single,expected_max_single = [2],[8]
+    expected_min_mult,expected_max_mult = [2,2,2,2,2],[8,8,8,8,8]
+    func_min_single,func_max_single = cmn.getColBounds(test_df,'col_1')
+    func_min_mult,func_max_mult = cmn.getColBounds(test_df,'col_1',5)
+    assert expected_min_single == func_min_single, """getColBounds min single failed"""
+    assert expected_max_single == func_max_single, """getColBounds max single failed"""
+    assert expected_min_mult == func_min_mult, """getColBounds min mult failed"""
+    assert expected_max_mult == func_max_mult, """getColBounds max mult failed"""
