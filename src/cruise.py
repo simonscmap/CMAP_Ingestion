6dfedd18788d187d8f85a46b77dc2422dc8ea260
raphael <norlandrhagen@gmail.com>
2020-05-12 16:03:14 -0700

    added functionallity to cruise for chief sci

--- src/cruise.py
+++ src/cruise.py
@@ -36,13 +36,37 @@ def retrieve_id_search(cmdf,id_col_str):
     id_return = cmdf[cmdf['id_col'].str.contains(id_col_str)]['info_col'].to_list()
     return id_return
 
+def trim_returned_link(link_str):
+    trimmed_link = [link.replace('<','').replace('>','') for link in link_str]
+    return trimmed_link
+
+def get_cruise_traj(cmdf):
+    cruise_traj_links = retrieve_id_search(cmdf,"r2r:hasProduct")# [0].replace('<','').replace('>','')
+    trim_traj_link = trim_returned_link(cruise_traj_links)
+    if len(trim_traj_link) > 1:
+        trim_link = trim_traj_link[1] # second cruise product should be 1 min temporal res
+    else:
+        trim_link = trim_traj_link[0]
+    cruise_traj_df = parse_r2r_page(trim_link)
+    label = retrieve_id_search(cruise_traj_df,"rdfs:label")
+    if label[0].contains("1Min"):
+        return
+
+    return label
+
+def get_chief_sci(cmdf):
+    chief_sci_link = retrieve_id_search(cmdf,"r2r:hasParticipant")[0].replace('<','').replace('>','')
+    chief_sci_df = parse_r2r_page(chief_sci_link)
+    chief_sci = retrieve_id_search(chief_sci_df,"rdfs:label")
+    chief_sci = chief_sci[0].split(" on")[0]
+    return chief_sci
+
 
 ########## Cruise Data Parsing ###################
 """"""
-def parse_cruise_metadata(cruise_name='', cruise_link=''):
-    if cruise_name != '':
-        cruise_url = "http://data.rvdata.us/page/cruise/" + cruise_name.upper()
-    page = requests.get(cruise_url)
+
+def parse_r2r_page(url):
+    page = requests.get(url)
     soup = BeautifulSoup(page.content, 'html.parser')
     table_rows = soup.findAll('tr')
     cmdf = pd.DataFrame(columns=['id_col','info_col'])
@@ -53,9 +77,28 @@ def parse_cruise_metadata(cruise_name='', cruise_link=''):
         rowlen = [row.replace('\n','').strip() for row in rowlen]
         cmdf.loc[len(cmdf)] = rowlen
     return cmdf
-cruise_url = "http://data.rvdata.us/page/cruise/KM1906"
-cmdf = parse_cruise_metadata(cruise_name = 'kok1606')
+
+def parse_cruise_metadata(cruise_name='', cruise_link=''):
+    if cruise_name != '':
+        cruise_url = "http://data.rvdata.us/page/cruise/" + cruise_name.upper()
+    print(cruise_url)
+    cmdf = parse_r2r_page(cruise_url)
+    return cmdf
+
+# cruise_url = "http://data.rvdata.us/page/cruise/KM1906"
+cmdf = parse_cruise_metadata(cruise_name = 'mgl1704')
+# cmdf = parse_cruise_metadata(cruise_name = 'km1513')
+
 cruise_name = retrieve_id_search(cmdf, "gl:hasCruiseID")
 cruise_nickname = retrieve_id_search(cmdf, "dcterms:title")
 cruise_shipname = retrieve_id_search(cmdf, "r2r:VesselName")
-chief_sci = retrieve_id_search(cmdf,"r2r:hasParticipant")
+chief_sci = get_chief_sci(cmdf)
+
+ctl = get_cruise_traj(cmdf)
+def print_cruise_info(cruise_name, cruise_nickname,cruise_shipname,chief_sci):
+    print("Cruise Name: ", cruise_name, "\n")
+    print("Cruise Nickname: ", cruise_nickname, "\n")
+    print("Cruise Ship Name: ", cruise_shipname, "\n")
+    print("Cruise Chief Scientist: ", chief_sci, "\n")
+
+# print_cruise_info(cruise_name, cruise_nickname,cruise_shipname,chief_sci)
