40099182d6ac13540c03c803f612bdf95cb9bf5c
raphael <norlandrhagen@gmail.com>
2020-05-27 11:39:27 -0700

    added req.txt and setup

--- conf/vault_structure.py
+++ conf/vault_structure.py
@@ -3,90 +3,91 @@
 import os
 from pathlib import Path
 
+
 def makedir(directory):
-	if not os.path.exists(directory):
-		os.makedirs(directory)
-	return
+    if not os.path.exists(directory):
+        os.makedirs(directory)
+    return
+
 
 def leafStruc(base):
-	makedir(base)
-	if base[-1] != '/':
-		base = base + '/'
-	nrt = base + 'nrt/'
-	makedir(nrt)
-	rep = base + 'rep/'
-	makedir(rep)
-	metadata = base + 'metadata/'
-	makedir(metadata)
-	stats = base + 'stats/'
-	makedir(stats)
-	doc = base + 'doc/'
-	makedir(doc)
-	code = base + 'code/'
-	makedir(code)
-	return nrt, rep, metadata, stats, doc, code
+    makedir(base)
+    if base[-1] != "/":
+        base = base + "/"
+    nrt = base + "nrt/"
+    makedir(nrt)
+    rep = base + "rep/"
+    makedir(rep)
+    metadata = base + "metadata/"
+    makedir(metadata)
+    stats = base + "stats/"
+    makedir(stats)
+    doc = base + "doc/"
+    makedir(doc)
+    code = base + "code/"
+    makedir(code)
+    return nrt, rep, metadata, stats, doc, code
 
-vault = str(Path.home()) + r'/CMAP Data Submission Dropbox/Simons CMAP/vault/'
-staging = str(Path.home()) + r'/AWS_staging/'
+
+vault = str(Path.home()) + r"/CMAP Data Submission Dropbox/Simons CMAP/vault/"
+staging = str(Path.home()) + r"/AWS_staging/"
 
 ################# AWS Staging Area Structure  ##################
 makedir(staging)
 
 #########  data  #########
-data = staging + 'data/'
+data = staging + "data/"
 makedir(data)
 
 ########  metadata  ########
-metadata = staging + 'metadata/'
+metadata = staging + "metadata/"
 makedir(metadata)
 
 ########  combined (temporary)  ########
-combined = staging + 'combined/'
+combined = staging + "combined/"
 makedir(combined)
-
-
 
 
 ################## Simons CMAP vault structure ##################
 makedir(vault)
 
 #########  BCP  #########
-BCP = vault + 'BCP/'
+BCP = vault + "BCP/"
 makedir(BCP)
 
 #########  assimilation  #########
-assimilation = vault + 'assimilation/'
+assimilation = vault + "assimilation/"
 makedir(assimilation)
 
 
 ###########  models  ##########
-model = vault + 'model/'
+model = vault + "model/"
 makedir(model)
 
 #########  r2r2 cruise #########
-r2r_cruise = vault + 'r2r_cruise/'
+r2r_cruise = vault + "r2r_cruise/"
 makedir(r2r_cruise)
 
 #########  observations  #########
-obs = vault + 'observation/'
+obs = vault + "observation/"
 makedir(obs)
-in_situ = obs + 'in-situ/'
+in_situ = obs + "in-situ/"
 makedir(in_situ)
-remote = obs + 'remote/'
+remote = obs + "remote/"
 makedir(remote)
 
 #########  obs/in-situ/cruise  #########
-cruise = in_situ + 'cruise/'
+cruise = in_situ + "cruise/"
 makedir(cruise)
 
 #########  obs/in-situ/station  #########
-station = in_situ + 'station/'
+station = in_situ + "station/"
 makedir(station)
 
 #########  obs/in-situ/float  #########
-float = in_situ + 'float/'
+float = in_situ + "float/"
 makedir(float)
 
 ########  obs/remote/satellite  ########
-satellite = remote + 'satellite/'
+satellite = remote + "satellite/"
 makedir(satellite)
---
+++ setup.py
@@ -0,0 +1,15 @@
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+version = "0.1"
+
+setup(
+    name="cmapingest",
+    version=version,
+    install_requires=requirements,
+    author="Norland Raphael Hagen",
+    author_email="norlandrhagen@gmail.com",
+    packages=find_packages(),
+    include_package_data=True,
+    url="https://github.com/simonscmap/CMAP_Ingestion",
+)
